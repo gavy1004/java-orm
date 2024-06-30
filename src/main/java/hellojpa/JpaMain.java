4c114250a17b1b382a4c54aab068cf15d23c1d0d
gavy1004 <avanetkr22@gmail.com>
2024-06-30 23:46:50 +0900

    feat : 기본키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,23 +11,58 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
         // 트랜잭션 안에서 작업 필수
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            // 비영속 상태
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("helloJPA");
+            /*
+            // 영속
+            Member findMember1 = em.find(Member.class, 101L);
+            // 1차 캐시에서 조회하므로 쿼리 나가지 않음
+            Member findMember2 = em.find(Member.class, 101L);
+            // 동일한 객체 주소 참조
+            System.out.println("(findMember1 == findMember2) = " + (findMember1 == findMember2));
+            */
 
-            // 영속 상태
-            // 영속성 컨텍스트 안에서 관리되는 상태
+            // insert 쿼리 모아서 한번에 insert
+            //Member mem1 = new Member(130L, "test");
+            //Member mem2 = new Member(131L, "test");
+            //em.persist(mem1);
+            //em.persist(mem2);
+            //System.out.println(" ================= ");
+            // batch
+
+            // 엔티티 수정 변경감지 (Dirty Checking)
+            //Member member = em.find(Member.class, 130L);
+            //member.setName("zzz");
+            // em.update(member) 라는 코드 없어도 됨
+
+            // 엔티티 삭제
+            // em.remove(member);
+
+            // 플러시
+            // 영속성 컨텍스트의 변경내용을 데이터 베이스에 반영
+            // 영속성 컨텍스트를 비우지 않음 
+            // 커밋직전에만 동기화 하면됨
+            // 영속성 컨텍스트를 플러시 하는 방법
+            // em.fluch(); 직접 호출
+            // tx.commit(); 시 자동 호출
+            //Member member = new Member(200L, "dfs200");
+            //em.persist(member);
+            //em.flush();
+            //System.out.println(" ================= ");
+            //tx.commit();
+
+            // 준영속 상태
+
+            Member member = new Member();
+            member.setId(142L);
+            member.setName("C");
+
+            member.setRoleType(RoleType.GUEST);
+
             em.persist(member);
-            // 1차 캐시에 저장되어있으므로 select 쿼리 나가지 않음
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember.getId() = " + findMember.getId());
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,9 +1,10 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "MEMBER")
@@ -12,8 +13,33 @@ public class Member {
     @Id
     @Column(name = "ID")
     private Long id;
+
     @Column(name = "NAME")
-    private String name;
+    private String username;
+
+    private Integer age;
+
+    // ORDINAL 사용 X
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // DATA, TIME, TIMESTAMP 지정
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // 문자일시 CLOB, 나머지는 BLOB 매핑
+    @Lob
+    private String description;
+
+    // 필드매핑 X, 메모리에서만 사용
+    @Transient
+    private int temp;
 
     public Long getId() {
         return id;
@@ -24,10 +50,66 @@ public class Member {
     }
 
     public String getName() {
-        return name;
+        return username;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.username = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
